cd2ab0c9268b994590990799e62938ca3c71a864
nAoMii-hihi <nkmlbelleza@brc.pshs.edu.ph>
2026-09-05 19:04:05 +0800

    Add score validation and classification for input
    Implement score validation and classification logic.

--- q1/score_checker.py
+++ q1/score_checker.py
@@ -1,1 +1,16 @@
+# Ask the user to enter a student score
+score = int(input("Enter a score: "))
 
+# The score should be within the range
+if score < 0 or score > 100:
+  print("Invalid score.")
+
+# Sort the scores by their classfication
+elif score >= 90:
+  print("Outstanding!")
+elif score >= 80:
+  print("Very Satisfactory!")
+elif score >= 75:
+  print("Satisfactory!")
+else:
+  print("Needs improvement.")
